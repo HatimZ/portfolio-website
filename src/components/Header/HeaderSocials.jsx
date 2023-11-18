e94d872d1351124c00947413e387646f961359d6
Hatim Zahid <63000127+HatimZ@users.noreply.github.com>
2023-11-18 17:40:38 -0500

    Update HeaderSocials.jsx
    Added Upwork in Socials

--- src/components/Header/HeaderSocials.jsx
+++ src/components/Header/HeaderSocials.jsx
@@ -3,9 +3,13 @@ import {BsLinkedin} from 'react-icons/bs'
 import {AiFillGithub} from 'react-icons/ai'
 import {FaMedium} from 'react-icons/fa'
 import {BsStackOverflow} from 'react-icons/bs'
+import { SiUpwork } from "react-icons/si";
 const HeaderSocials = () => {
   return (
     <div className="header__socials">
+             <a href="https://www.linkedin.com/in/hatim-zahid/" target="_blank"><SiUpwork size={23} /></a>
+
+       <a href="https://www.upwork.com/freelancers/~015dba6905ee11ad1a" target="_blank"><BsLinkedin size={23} /></a>
         <a href="https://www.linkedin.com/in/hatim-zahid/" target="_blank"><BsLinkedin size={23} /></a>
         <a href="https://github.com/HatimZ" target="_blank"><AiFillGithub size={23}/></a>
         <a href="https://medium.com/@hatim.zahid"target="_blank" ><FaMedium size={23}/></a>
